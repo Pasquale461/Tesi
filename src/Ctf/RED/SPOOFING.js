7579a8d5cf06aebb0469f3ab55794228066b8ac8
Gius3ppe1 <g.moramarco20@studenti.uniba.it>
2023-09-23 17:26:32 +0200

    Spoofing finished

--- src/Ctf/RED/SPOOFING.js
+++ src/Ctf/RED/SPOOFING.js
@@ -3,7 +3,7 @@ import React from 'react';
 function SPOOFING() {
   return (
     <div id="sql-injection">
-      <h1 className='title'>SQL Injection</h1>
+      <h1 className='title'>SPOOFING</h1>
       <div class="attack-phase">
         <h2>1. Reconnaissance (Ricerca)</h2>
 
@@ -23,7 +23,7 @@ function SPOOFING() {
           
         
 
-        <p>L'attaccante raccoglie informazioni sulla rete IoT o sulla città intelligente, inclusi i tipi di sensori utilizzati, i protocolli di comunicazione e le modalità di raccolta dati.</p>
+        <p>L'attaccante inizia raccogliendo informazioni sulla rete Wi-Fi pubblica della Smart City, inclusi i nomi delle reti (SSID), le posizioni degli access point e le pratiche di autenticazione utilizzate. Queste informazioni sono ottenute attraverso la scansione delle reti Wi-Fi e l'analisi dei dati di rete.</p>
       </div>
       <div class="attack-phase">
         <h2>2. Weaponization (Armatizzazione)</h2>
@@ -32,7 +32,7 @@ function SPOOFING() {
 <path fill-rule="evenodd" clip-rule="evenodd" d="M19 21C19 21.5523 18.5523 22 18 22H14H10H6C5.44771 22 5 21.5523 5 21V18.75C5 17.7835 4.2165 17 3.25 17C2.55964 17 2 16.4404 2 15.75V11C2 5.47715 6.47715 1 12 1C17.5228 1 22 5.47715 22 11V15.75C22 16.4404 21.4404 17 20.75 17C19.7835 17 19 17.7835 19 18.75V21ZM17 20V18.75C17 16.9358 18.2883 15.4225 20 15.075V11C20 6.58172 16.4183 3 12 3C7.58172 3 4 6.58172 4 11V15.075C5.71168 15.4225 7 16.9358 7 18.75V20H9V18C9 17.4477 9.44771 17 10 17C10.5523 17 11 17.4477 11 18V20H13V18C13 17.4477 13.4477 17 14 17C14.5523 17 15 17.4477 15 18V20H17ZM11 12.5C11 13.8807 8.63228 15 7.25248 15C5.98469 15 5.99206 14.055 6.00161 12.8306V12.8305C6.00245 12.7224 6.00331 12.6121 6.00331 12.5C6.00331 11.1193 7.12186 10 8.50166 10C9.88145 10 11 11.1193 11 12.5ZM17.9984 12.8306C17.9975 12.7224 17.9967 12.6121 17.9967 12.5C17.9967 11.1193 16.8781 10 15.4983 10C14.1185 10 13 11.1193 13 12.5C13 13.8807 15.3677 15 16.7475 15C18.0153 15 18.0079 14.055 17.9984 12.8306Z" fill="#000000"/>
 </svg>
        
-        <p> L'attaccante sceglie il sensore o il gruppo di sensori che desidera influenzare con dati falsi.</p>
+        <p> Identifica le vulnerabilità nella configurazione e nella sicurezza della rete Wi-Fi pubblica della Smart City. Scopre che alcuni access point sono configurati in modo debole e che le reti utilizzano metodi di autenticazione non sufficientemente sicuri.</p>
       </div>
       <div class="attack-phase">
         <h2>3. Delivery (Consegna)</h2>
@@ -42,7 +42,7 @@ function SPOOFING() {
 <path d="M10.5 8.00012L13.5 11.0001M13.5 8.00012L10.5 11.0001M3.02832 10.0001L10.2246 14.8167C10.8661 15.2444 11.1869 15.4582 11.5336 15.5413C11.8399 15.6147 12.1593 15.6147 12.4657 15.5413C12.8124 15.4582 13.1332 15.2444 13.7747 14.8167L20.971 10.0001M10.2981 4.06892L4.49814 7.71139C3.95121 8.05487 3.67775 8.2266 3.4794 8.45876C3.30385 8.66424 3.17176 8.90317 3.09111 9.16112C3 9.45256 3 9.77548 3 10.4213V16.8001C3 17.9202 3 18.4803 3.21799 18.9081C3.40973 19.2844 3.71569 19.5904 4.09202 19.7821C4.51984 20.0001 5.0799 20.0001 6.2 20.0001H17.8C18.9201 20.0001 19.4802 20.0001 19.908 19.7821C20.2843 19.5904 20.5903 19.2844 20.782 18.9081C21 18.4803 21 17.9202 21 16.8001V10.4213C21 9.77548 21 9.45256 20.9089 9.16112C20.8282 8.90317 20.6962 8.66424 20.5206 8.45876C20.3223 8.2266 20.0488 8.05487 19.5019 7.71139L13.7019 4.06891C13.0846 3.68129 12.776 3.48747 12.4449 3.41192C12.152 3.34512 11.848 3.34512 11.5551 3.41192C11.224 3.48747 10.9154 3.68129 10.2981 4.06892Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
        
-        <p>L'attaccante crea dati falsificati che sembrano provenire da un sensore legittimo. Questi dati possono essere manipolati per mostrare valori errati o fuorvianti.</p>
+        <p>L'attaccante configura un access point Wi-Fi fasullo con un nome (SSID) simile o identico a una delle reti Wi-Fi pubbliche della Smart City. Questo access point sarà sotto il controllo dell'attaccante e verrà utilizzato per ingannare gli utenti.</p>
       </div>
       <div class="attack-phase">
         <h2>4. Exploitation (Sfruttamento)</h2>
@@ -50,7 +50,7 @@ function SPOOFING() {
         <svg fill="#000000" width="8%" height="8%" viewBox="0 0 64 64" data-name="Material Expand" id="Material_Expand" xmlns="http://www.w3.org/2000/svg"><path d="M6.188,52.173A2,2,0,0,0,5,54v8H9V55.3l6.992-3.108a17.968,17.968,0,0,0,32.016,0L55,55.3V62h4V54a2,2,0,0,0-1.188-1.827l-8.39-3.729a17.858,17.858,0,0,0,.511-3.115l7.879-3.5A2,2,0,0,0,59,40V32H55v6.7l-5,2.223V30.3l7.812-3.472A2,2,0,0,0,59,25V17H55v6.7l-5.325,2.367A6.006,6.006,0,0,0,44,22H42V18a7.941,7.941,0,0,0-.99-3.847l3.239-2.591A2,2,0,0,0,45,10V5H41V9.039l-2.747,2.2A7.939,7.939,0,0,0,34,10H30a7.939,7.939,0,0,0-4.253,1.237L23,9.039V5H19v5a2,2,0,0,0,.751,1.562l3.239,2.591A7.941,7.941,0,0,0,22,18v4H20a6.006,6.006,0,0,0-5.675,4.067L9,23.7V17H5v8a2,2,0,0,0,1.188,1.827L14,30.3V40.923L9,38.7V32H5v8a2,2,0,0,0,1.188,1.827l7.879,3.5a17.858,17.858,0,0,0,.511,3.115ZM18,44V34H30V57.84A14.009,14.009,0,0,1,18,44ZM34,57.84V34H46V44A14.009,14.009,0,0,1,34,57.84ZM26,18a4,4,0,0,1,4-4h4a4,4,0,0,1,4,4v4H26Zm-6,8H44a2,2,0,0,1,2,2v2H18V28A2,2,0,0,1,20,26Z"/></svg>
 
 
-<p>I dati contraffatti vengono inseriti nella rete IoT o nella città intelligente. L'attaccante può utilizzare varie tecniche per iniettare questi dati falsi nella rete, come la sostituzione dei dati reali con quelli contraffatti o l'invio diretto ai dispositivi di raccolta dati.</p>      </div>
+<p> Colloca fisicamente l'access point fasullo in una zona frequentata della Smart City, come una piazza o un centro commerciale. L'obiettivo è attirare gli utenti a connettersi a questo access point invece di quello legittimo.</p>      </div>
       <div class="attack-phase">
         <h2>5. Installation (Installazione)</h2>
        
@@ -59,7 +59,7 @@ function SPOOFING() {
 </svg>
 
 
-<p> I dati falsificati vengono elaborati e utilizzati dai dispositivi o dalle piattaforme che dipendono dai dati provenienti da quei sensori. Ad esempio, se si tratta di dati di qualità dell'aria, le decisioni sulla salute pubblica o le notifiche di inquinamento potrebbero basarsi su questi dati falsi.</p>      </div>
+<p> Gli utenti della Smart City, ignari del pericolo, si connettono all'access point fasullo dell'attaccante, pensando che sia una rete Wi-Fi legittima. Una volta connessi, l'attaccante può intercettare il traffico di rete, raccogliere informazioni sensibili.</p>      </div>
       <div class="attack-phase">
         <h2>6. Command and Control (Comando e Controllo)</h2>
         <svg width="8%" height="8%" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
@@ -76,15 +76,14 @@ function SPOOFING() {
     </g>
   </g>
 </svg>
-<p>L'attaccante può monitorare i risultati dell'attacco e decidere se continuare a fornire dati falsi o se interrompere l'attacco. Questo può variare a seconda degli obiettivi dell'attaccante.</p>      </div>
+<p>L'attaccante stabilisce il controllo sull'access point fasullo per continuare a intercettare il traffico di rete e raccogliere dati sensibili. Può anche utilizzare questa posizione per eseguire ulteriori attacchi, come l'invio di malware o il furto di credenziali.</p>      </div>
       <div class="attack-phase">
         <h2>7. Actions on Objectives (Azioni sugli Obiettivi)</h2>
         <svg width="8%" height="8%" viewBox="-0.5 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M12 22.9199C17.5228 22.9199 22 18.4428 22 12.9199C22 7.39707 17.5228 2.91992 12 2.91992C6.47715 2.91992 2 7.39707 2 12.9199C2 18.4428 6.47715 22.9199 12 22.9199Z" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M12.7002 17.1099V18.21C12.7002 18.3877 12.6296 18.5582 12.504 18.6838C12.3783 18.8095 12.2079 18.8799 12.0302 18.8799C11.8525 18.8799 11.6821 18.8095 11.5565 18.6838C11.4308 18.5582 11.3602 18.3877 11.3602 18.21V17.0801C10.9165 17.0072 10.4917 16.8468 10.1106 16.6082C9.72943 16.3695 9.39958 16.0573 9.14023 15.6899C9.04577 15.57 8.99311 15.4226 8.99023 15.27C8.99014 15.1834 9.00762 15.0975 9.04164 15.0178C9.07566 14.9382 9.12551 14.8662 9.18816 14.8064C9.2508 14.7466 9.32494 14.7 9.40608 14.6697C9.48723 14.6393 9.5737 14.6258 9.66023 14.6299C9.74611 14.6294 9.83102 14.648 9.90884 14.6843C9.98667 14.7206 10.0554 14.774 10.1102 14.8401C10.4301 15.258 10.8643 15.574 11.3602 15.75V13.21C10.0302 12.69 9.36023 11.9099 9.36023 10.8999C9.38027 10.3592 9.59279 9.84343 9.95949 9.44556C10.3262 9.04769 10.8229 8.79397 11.3602 8.72998V7.62988C11.3602 7.45219 11.4308 7.2819 11.5565 7.15625C11.6821 7.0306 11.8525 6.95996 12.0302 6.95996C12.2079 6.95996 12.3783 7.0306 12.504 7.15625C12.6296 7.2819 12.7002 7.45219 12.7002 7.62988V8.71997C13.0723 8.77828 13.4289 8.91103 13.7485 9.11035C14.0681 9.30967 14.3442 9.57137 14.5602 9.87988C14.6555 9.99235 14.7117 10.1329 14.7202 10.28C14.7229 10.3657 14.7083 10.451 14.6774 10.531C14.6464 10.611 14.5997 10.684 14.54 10.7456C14.4803 10.8072 14.4088 10.856 14.3298 10.8894C14.2509 10.9228 14.166 10.94 14.0802 10.9399C13.9906 10.9394 13.9022 10.9196 13.8211 10.8816C13.74 10.8436 13.668 10.7884 13.6102 10.72C13.3718 10.4221 13.0574 10.1942 12.7002 10.0601V12.3101L12.9502 12.4099C14.2202 12.9099 15.0102 13.63 15.0102 14.77C14.9954 15.3808 14.7481 15.9629 14.3189 16.3977C13.8897 16.8325 13.3108 17.0871 12.7002 17.1099ZM11.3602 11.73V10.0999C11.1988 10.1584 11.0599 10.2662 10.963 10.408C10.8662 10.5497 10.8162 10.7183 10.8202 10.8899C10.8185 11.0673 10.8688 11.2414 10.9647 11.3906C11.0607 11.5399 11.1981 11.6579 11.3602 11.73ZM13.5502 14.8C13.5502 14.32 13.2202 14.03 12.7002 13.8V15.8C12.9387 15.7639 13.156 15.6427 13.3122 15.459C13.4684 15.2752 13.553 15.0412 13.5502 14.8Z" fill="#000000"/>
 </svg>
-        <p>L'obiettivo di uno spoofing dei dati dei sensori può variare, ma generalmente mira a influenzare le decisioni o a creare confusione all'interno del sistema IoT o della città intelligent</p>
-      <img src={require('../../images/spoofingex.png')} style={{borderRadius : '20px'}}/>
+        <p>L'attaccante può sfruttare le informazioni raccolte per scopi malevoli, come il furto di dati personali, finanziari o sensibili. Questi dati possono essere utilizzati per il ricatto, l'identità fraudolenta o altri scopi dannosi.</p>
       </div>
     </div>
   );
